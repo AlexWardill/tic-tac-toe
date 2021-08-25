db75dd463a99d7e0e41f5ac5bf707643fd8f4f7e
Alex Wardill <alexander@wardill.net>
2021-08-25 18:55:50 +0100

    added checkLines()

--- script.js
+++ script.js
@@ -3,33 +3,41 @@ const squares = [...document.querySelectorAll('.board-square')];
 
 const Gb = (function() {
     return {
-        top : [1, 2, 3],
-        middle : [4, 5, 5],
-        bottom : [7, 8, 9],
-        left : [1, 4, 7],
-        centre : [2, 5, 8],
-        right : [3, 6, 9],
-        downdiag : [1, 5, 9],
-        updiag : [7, 5, 3],
+            lines : {
+                'top' : [1, 2, 3],
+                'middle' : [4, 5, 6],
+                'bottom' : [7, 8, 9],
+                'left' : [1, 4, 7],
+                'centre' : [2, 5, 8],
+                'right' : [3, 6, 9],
+                'downdiag' : [1, 5, 9],
+                'updiag' : [7, 5, 3],
+            },
 
         board : [
             '','','',
             '','','',
             '','',''
         ],
-
-        getLine(arr = this.board, line) {
-            return line.map(i => arr[i-1]);
+        getLine(line) {
+            return Gb.lines[line];
         }
     
     }
 })();
 
-function checkLines() {
-    const cwin = [1,1,1];
-    const nwin = [0,0,0];
-
-
+// Check for 3 in a row
+function checkLines(line) {
+    const cwin = [1,1,1].toString();
+    const nwin = [0,0,0].toString();
+    console.log('lines checked');
+    let lineToCheck = line.map(index => Gb.board[index-1]).toString();
+    if (lineToCheck == cwin || lineToCheck == nwin) {
+       console.log('We Have A Winner!');
+       console.log(line.map(index => Gb.board[index-1]));
+       line.map(index => document.getElementById(`${index.toString()}`).classList.add('winner'));
+    } //
+    
 }
 
 
@@ -41,8 +49,10 @@ squares.forEach(square => {
         }
         console.log(Gb.board);
 
-
-
+        // Every click, check all the lines
+        // CheckLines sees if any 3 in a row
+        Object.values(Gb.lines)
+            .forEach(line => checkLines(line));
 
 
 
