fdc3c026711ceebda0a3470980e6ada2704e5456
Alex Wardill <alexander@wardill.net>
2021-08-25 12:57:37 +0100

    add board array

--- script.js
+++ script.js
@@ -1,13 +1,53 @@
 let crosses = true;
 const squares = [...document.querySelectorAll('.board-square')];
+
+const Gb = (function() {
+    return {
+        top : [1, 2, 3],
+        middle : [4, 5, 5],
+        bottom : [7, 8, 9],
+        left : [1, 4, 7],
+        centre : [2, 5, 8],
+        right : [3, 6, 9],
+        downdiag : [1, 5, 9],
+        updiag : [7, 5, 3],
+
+        board : [
+            '','','',
+            '','','',
+            '','',''
+        ],
+
+        getLine(arr = this.board, line) {
+            return line.map(i => arr[i-1]);
+        }
+    
+    }
+})();
+
+function checkLines() {
+    const cwin = [1,1,1];
+    const nwin = [0,0,0];
+
+
+}
+
+
 squares.forEach(square => {
     square.addEventListener('click', (e) => {
-        console.log(crosses, e.target)
-        crosses ? Gameboard[`sq${e.target.id}`] = 'cross' : 'naught'; 
-        console.log(Gameboard);
+        // crosses ? Gb[`sq${e.target.id}`] = 'cross' : Gb[`sq${e.target.id}`] ='naught'; 
+        if (Gb.board[e.target.id - 1] == '') {
+            crosses ? Gb.board[e.target.id - 1] = 1 : Gb.board[e.target.id - 1] = 0;
+        }
+        console.log(Gb.board);
+
+
+
+
+
+
         crosses = !crosses;
     })
 })
 
 
-const Gameboard = {}
